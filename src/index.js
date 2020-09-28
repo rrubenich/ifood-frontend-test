5564baf87241c6d0ca97e58bc4c1381c8a7e79c8
rubenich <rfrubenich@gmail.com>
2020-09-28 19:37:13 -0300

    chore: add and configure Material UI library and styled components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core";
+import MuiTheme from "./theming/MuiTheme";
 import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={MuiTheme}>
+      <StyledThemeProvider theme={MuiTheme}>Spotifood</StyledThemeProvider>
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById("root"),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
