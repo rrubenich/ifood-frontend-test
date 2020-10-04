7ca2e37e6a178756c01d6da21f9f133f70db4b55
rubenich <rfrubenich@gmail.com>
2020-10-04 01:27:18 -0300

    feat(i18n): add i18 library, pt_BR and en_US messages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core";
 import MuiTheme from "./theming/MuiTheme";
+import Home from "./pages/home";
+import "./i18n/i18n";
 import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <MuiThemeProvider theme={MuiTheme}>
-      <StyledThemeProvider theme={MuiTheme}>Spotifood</StyledThemeProvider>
-    </MuiThemeProvider>
-  </React.StrictMode>,
+  <MuiThemeProvider theme={MuiTheme}>
+    <StyledThemeProvider theme={MuiTheme}>
+      <Suspense fallback="loading">
+        <Home />
+      </Suspense>
+    </StyledThemeProvider>
+  </MuiThemeProvider>,
   document.getElementById("root"),
 );
